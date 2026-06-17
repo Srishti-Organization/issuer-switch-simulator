112af7910736308e48388f1dd86f3344340e57a1
srishti-rao <srishtirao7@gmail.com>
2026-06-17 17:55:35 +0545

    Add: fielddefinition.java

--- src/iso/FieldDefinition.java
+++ src/iso/FieldDefinition.java
@@ -1,4 +1,36 @@
 package iso;
 
-public class FieldDefinition {
+public final class FieldDefinition {
+    private final int id;
+    private final FieldType type;
+    private final int length;
+    private final String label;
+
+    public FieldDefinition(int id, FieldType type, int length, String label) {
+        this.id = id;
+        this.type = type;
+        this.length = length;
+        this.label = label;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public FieldType type() {
+        return type;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public String label() {
+        return label;
+    }
+
+    @Override
+    public String toString() {
+        return "DE" + id + "(" + label + "," + type + "," + length + ")";
+    }
 }
