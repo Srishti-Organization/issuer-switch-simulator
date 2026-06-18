fac8a06e83c687f72d331dd4c15577da7931d8e2
srishti-rao <srishtirao7@gmail.com>
2026-06-18 11:40:41 +0545

    Init: ResponseCode.java

--- src/processing/ResponseCode.java
+++ src/processing/ResponseCode.java
@@ -1,4 +1,13 @@
 package processing;
 
-public class ResponseCode {
+public final class ResponseCode {
+    public static final String APPROVED = "00";
+    public static final String INVALID_TRANSACTION = "12";
+    public static final String INVALID_CARD = "14";
+    public static final String UNABLE_TO_LOCATE_RECORD = "25";
+    public static final String INSUFFICIENT_FUNDS = "51";
+    public static final String SYSTEM_MALFUNCTION = "96";
+
+    private ResponseCode() {
+    }
 }
